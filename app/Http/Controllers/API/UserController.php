7204fecec172f3f644adad3f20938ffbcbeaa4ec
basilmuhammad91 <basilmuhammad91@gmail.com>
2022-10-16 19:16:27 +0500

    Axios/Sanctum Setup

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        // $this->middleware(['auth', 'permission:users']);
+        // $this->middleware(['auth:sanctum']);
+    }
+
+    public function index()
+    {
+        return User::get();
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/js/pages/users.vue
+++ resources/js/pages/users.vue
@@ -3,3 +3,14 @@
         Users
     </h1>
 </template>
+
+<script>
+export default {
+    mounted() {
+        axios.get('api/users')
+        .then((response) => {
+            console.log(response.data)
+        })
+    },
+}
+</script>
